d5246e043d6893e06c8c9cb6dacb0bc5351e771f
Max Struever <struever.max@bcgplatinion.com>
2021-06-15 13:45:36 -0400

    fix(): url

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,14 +5,7 @@ import { jwtVerify } from 'jose/jwt/verify';
 @Injectable()
 export class AuthService {
   constructor() {}
-  JWKS = createRemoteJWKSet(
-    new URL(
-      // process.env.AAD_JWKS || 'https://login.microsoftonline.com/common/discovery/keys'
-      process.env.AAD_JWKS ||
-        // 'https://devfmgidp.b2clogin.com/devfmgidp.onmicrosoft.com/discovery/v2.0/keys?p=b2c_1a_signup_signin'
-        'https://sbxfmgidp.b2clogin.com/sbxfmgidp.onmicrosoft.com/discovery/v2.0/keys?p=B2C_1A_signup_signin'
-    )
-  );
+  JWKS = createRemoteJWKSet(new URL(process.env.AAD_JWKS || 'https://login.microsoftonline.com/common/discovery/keys'));
 
   async validateToken(token: string, allowedRoles: string[]) {
     try {
